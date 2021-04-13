b10a4d4bdb6cd1482f08b4b7d4614eaf021c70ce
alemi <me@alemi.dev>
2021-04-13 11:53:08 +0200

    Added POST route for webhook

--- telegithook/server/routes/index.py
+++ telegithook/server/routes/index.py
@@ -1,12 +1,16 @@
 from fastapi import APIRouter, Request
 
-from messages import send_raw_event
+from bot.messages import send_raw_event
 
 router = APIRouter()
 
 
 @router.get("/")
 async def index(req: Request):
+    return {"error": "you should send events as POST"}
+
+@router.post("/")
+async def webhook_base(req: Request):
     doc = await req.json()
     await send_raw_event(-445530430, doc)
     return {"result": "OK"}
