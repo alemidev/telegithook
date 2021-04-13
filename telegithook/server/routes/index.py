b229b0bb6829f66d6c3654374e879107cac1e467
alemi <me@alemi.dev>
2021-04-13 11:46:01 +0200

    [TEMPORARY] relay all to hardcoded channel

--- telegithook/server/routes/index.py
+++ telegithook/server/routes/index.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, Request
 
+from messages import send_raw_event
+
 router = APIRouter()
 
 
 @router.get("/")
 async def index(req: Request):
-    return "Ciao, cîhan!"
+    doc = await req.json()
+    await send_raw_event(-445530430, doc)
+    return {"result": "OK"}
