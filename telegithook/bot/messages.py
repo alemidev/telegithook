f0855a897cae323fa9dcaae414cccacc3c86cc1e
alemi <me@alemi.dev>
2021-04-13 11:45:48 +0200

    Added send_raw_event to relay a json file

--- telegithook/bot/messages.py
+++ telegithook/bot/messages.py
@@ -1,5 +1,13 @@
+import io
+import json
+
 from . import bot
 
 
 async def send_message(chat_id: int, message: str):
     await bot.send_message(chat_id, message)
+
+async def send_raw_event(chat_id: int, event: dict, caption: str =""):
+    doc = io.BytesIO(json.dumps(event, indent=2).encode('utf-8'))
+    doc.name = "event.json"
+    await bot.send_document(chat_id=chad_id, document=doc, caption=caption)
