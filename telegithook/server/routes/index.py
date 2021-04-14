f1a41d00c3dbcd5b5b9aa16fbb98098404d9006f
Roj Serbest <rojserbest@gmail.com>
2021-04-15 01:13:32 +0300

    a

--- telegithook/server/routes/index.py
+++ telegithook/server/routes/index.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Request
 
 from ...bot.messages import send_message, send_raw_event
 from ..helpers import to_html
-from ...actions import actions
 from ...events import EVENTS
 from ...env import get
 
@@ -18,14 +17,11 @@ async def _(__):
 async def webhook_base(req: Request):
     json = await req.json()
 
-    for action in actions:
-        if action in json:
-            await send_message(get("CHAT_ID"), actions[action].format(r=json, html=to_html))
-
-    text = "" # TODO fancier message/string builder
+    text = ""  # TODO fancier message/string builder
     for event in EVENTS:
         if event.isPresent(json):
             text += event(json).parse()
     await send_message(get("CHAT_ID"), text)
 
-    await send_raw_event(get("CHAT_ID"), json) # still send this for debug purposes
+    # still send this for debug purposes
+    await send_raw_event(get("CHAT_ID"), json)
