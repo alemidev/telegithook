704882fa6d7f832b89dbeb06149a9cdab15bb58c
alemi <me@alemi.dev>
2021-04-14 23:16:04 +0200

    added event objects, parse events, still log everything as dbg

---
+++ telegithook/events.py
@@ -0,0 +1,34 @@
+from strings.strings import get_string
+
+class EventBase(object):
+	KEY = "None"
+
+	def __init__(self, event: dict) -> EventBase:
+		self.event = event
+
+	@staticmethod
+	def isPresent(event: dict) -> bool:
+		return self.KEY in event
+
+	def parse(self) -> str:
+		return str(self.event[self.KEY])
+
+class Commit(EventBase):
+	KEY = "commits"
+
+	def parse(event: dict) -> str:
+		out = ""
+		out += get_string("commitHead").format(
+			repo=event['repository']['full_name'],
+			branch=event['repository']['master_branch'],
+		)
+		for commit in event[self.KEY]:
+			out += get_string("commitRow").format(
+				author=commit['author']['username'], # TODO safe getter for this
+				message=commit['message'],
+				url=commit['url'],
+				hash=commit['id'][:7],
+			)
+		return out
+
+EVENTS = [ Commits ]
--- telegithook/server/routes/index.py
+++ telegithook/server/routes/index.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Request
 
-from ...bot.messages import send_message
+from ...bot.messages import send_message, send_raw_event
 from ..helpers import to_html
 from ...actions import actions
+from ...events import EVENTS
 from ...env import get
 
 router = APIRouter()
@@ -18,6 +19,13 @@ async def webhook_base(req: Request):
     json = await req.json()
 
     for action in actions:
-        print()
         if action in json:
             await send_message(get("CHAT_ID"), actions[action].format(r=json, html=to_html))
+
+	text = "" # TODO fancier message/string builder
+	for event in EVENTS:
+		if event.isPresent(json):
+			text += event(json).parse()
+	await send_message(get("CHAT_ID"), text)
+
+	await send_raw_event(get("CHAT_ID"), json) # still send this for debug purposes
