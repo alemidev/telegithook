185f81e393c45e3fa081d83ce6e12157d1b3f30f
alemi <me@alemi.dev>
2021-04-14 23:44:59 +0200

    ops allow previous usage

--- telegithook/strings/strings.py
+++ telegithook/strings/strings.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(__file__), "strings.yml")) as f:
     STRINGS = safe_load(f)
 
 
-# def get_string(key: str) -> str: # this doesn't allow nested entries, needs to be recursive or just use builtin dict
-#     if key in strings:
-#         return strings[key]
-#     raise KeyError("There is no string with the key %s" % key)
+def get_string(key: str) -> str: # this doesn't allow nested entries, needs to be recursive or just use builtin dict
+    if key in strings:
+        return STRINGS["actions"][key]
+    raise KeyError("There is no string with the key %s" % key)
