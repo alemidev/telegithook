58325639ae5d6251d9ccc8b4a613b3dd51bbcf7e
Roj Serbest <rojserbest@gmail.com>
2021-04-14 18:21:04 +0300

    x

--- telegithook/actions.py
+++ telegithook/actions.py
@@ -1,8 +1,5 @@
 actions = {
     "forkee": (
-        "{html(r['sender']['login'], r['sender']['html_url'])}"
-        " forked"
-        " {html(r['repository']['name'], r['repository']['html_url'])}."
-        " Total forks now: {r['repository']['forks']}."
+        " {r'repository']['name']}"
     )
 }
