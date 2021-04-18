25082d6753936fba1ee826b1919bca1f643cb6e7
alemi <me@alemi.dev>
2021-04-18 17:25:46 +0200

    Added issue comment event

--- telegithook/events/issues.py
+++ telegithook/events/issues.py
@@ -12,12 +12,33 @@ class IssueOpened(EventBase):
 
     def parse(self) -> str:
         return self.header("issue opened") + open_str.format(
-                author=event["issue"]["user"]["login"],
-                url=event["issue"]["html_url"],
-                number=event["issue"]["number"],
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+                title=self.event["issue"]["title"],
             ) + body_str.format(
-                title=event["issue"]["title"],
-                text=event["issue"]["body"]
-                        if len(event["issue"]["body"]) < 100 else
-                     event["issue"]["body"][:100] + "...",
+                author=self.event["issue"]["user"]["login"],
+                text=self.event["issue"]["body"]
+                        if len(self.event["issue"]["body"]) < 100 else
+                     self.event["issue"]["body"][:100] + "...",
+            )
+
+class IssueComment(EventBase):
+    KEY = 'comment'
+    comment_str = STR["issues"]["comment"]
+    body_str = STR["issues"]["body"]
+    
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return 'issue' in event and cls.KEY in event and event["action"] == "created"
+
+    def parse(self) -> str:
+        return self.header("issue comment") + comment_str.format(
+                url=self.event["comment"]["html_url"],
+                number=self.event["comment"]["number"],
+                title=self.event["comment"]["title"],
+            ) + body_str.format(
+                author=self.event["comment"]["user"]["login"],
+                text=self.event["comment"]["body"]
+                        if len(self.event["comment"]["body"]) < 100 else
+                     self.event["comment"]["body"][:100] + "...",
             )
