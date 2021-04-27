e9a4ef6742d178d9dbb1c8640f37c272e56fa683
Roj <rojserbest@gmail.com>
2021-04-27 16:38:17 +0300

    Add EXCLUDE_EVENTS

--- telegithook/events/__init__.py
+++ telegithook/events/__init__.py
@@ -1,4 +1,12 @@
 from .code import Commit, Fork
 from .issues import IssueOpened, IssueComment, IssueLabeled, IssueAssigned
+from ..env import EXCLUDE_EVENTS
 
-EVENTS = [ Commit, Fork, IssueOpened, IssueComment, IssueLabeled, IssueAssigned ]
+_events = [ Commit, Fork, IssueOpened, IssueComment, IssueLabeled, IssueAssigned ]
+events = []
+
+for event in _events:
+    if type(event).__name__.lower() not in EXCLUDE_EVENTS:
+        events.append(event)
+
+EVENTS = events
