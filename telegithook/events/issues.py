835204f49f79d0453b0b5122887da75cc766ad56
Suraj Meena <surajmeena505@gmail.com>
2021-06-21 23:55:45 +0530

    almost all Issue associated events are parsed except milestoned, assignees(broken), label

--- telegithook/events/issues.py
+++ telegithook/events/issues.py
@@ -69,3 +69,167 @@ class IssueAssigned(EventBase):
                 assignee=self.event["assignee"]["login"],
             )
 
+class IssueEditedTitle(EventBase):
+    KEY = 'issue'
+    edited_str = STR["issues"]["edited_t"]
+
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "edited" and "title" in event["changes"]
+
+    def parse(self) -> str:
+        return self.header("issue title edited") + self.edited_str.format(
+                changes_title=self.event["changes"]["title"]["from"],
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+            )
+class IssueEditedBody(EventBase):
+    KEY = 'issue'
+    edited_str = STR["issues"]["edited_b"]
+
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "edited" and "body" in event["changes"]
+
+    def parse(self) -> str:
+        return self.header("issue body edited") + self.edited_str.format(
+                issue_url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+                comment_url=self.event["comment"]["html_url"],
+                comment_id=self.event["comment"]["id"],
+                changes_body=self.event["changes"]["body"]["from"],
+            )
+
+class IssueDeleted(EventBase):
+    KEY = 'issue'
+    edited_str = STR["issues"]["deleted"]
+
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "deleted"
+    def parse(self) -> str:
+        return self.header("issue deleted") + self.edited_str.format(
+                title=self.event["issue"]["title"],
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+            )
+
+class IssuePinned(EventBase):
+    KEY = 'issue'
+    edited_str = STR["issues"]["pinned"]
+
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "pinned"
+
+    def parse(self) -> str:
+        return self.header("issue pinned") + self.edited_str.format(
+                title=self.event["issue"]["title"],
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+        )
+
+class IssueUnpinned(EventBase):
+    KEY = 'issue'
+    edited_str = STR["issues"]["unpinned"]
+
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "unpinned"
+
+    def parse(self) -> str:
+        return self.header("issue unpinned") + self.edited_str.format(
+                title=self.event["issue"]["title"],
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+            )
+
+class IssueClosed(EventBase):
+    KEY = 'issue'
+    edited_str = STR["issues"]["closed"]
+
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "closed"
+
+    def parse(self) -> str:
+        return self.header("issue closed") + self.edited_str.format(
+                title=self.event["issue"]["title"],
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+            )
+
+class IssueReopened(EventBase):
+    KEY = 'issue'
+    edited_str = STR["issues"]["reopened"]
+
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "reopened"
+
+    def parse(self) -> str:
+        return self.header("issue reopened") + self.edited_str.format(
+                title=self.event["issue"]["title"],
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+            )
+
+class IssueUnassigned(EventBase):
+    KEY = 'issue'
+    assignee_str = STR["issues"]["unassigned"]
+    
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "unassigned"
+
+    def parse(self) -> str:
+        return self.header("issue unassigned") + self.assignee_str.format(
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+                assignee=self.event["assignee"]["login"],
+            )
+
+class IssueLocked(EventBase):
+    KEY = 'issue'
+    assignee_str = STR["issues"]["locked"]
+    
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "locked"
+
+    def parse(self) -> str:
+        return self.header("issue locked") + self.assignee_str.format(
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+                reason=self.event["issue"]["active_lock_reason"],
+            )
+
+class IssueUnlocked(EventBase):
+    KEY = 'issue'
+    assignee_str = STR["issues"]["unlocked"]
+    
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "unlocked"
+
+    def parse(self) -> str:
+        return self.header("issue unlocked") + self.assignee_str.format(
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+            )
+
+class IssueTransferred(EventBase):
+    KEY = 'issue'
+    assignee_str = STR["issues"]["transferred"]
+    
+    @classmethod
+    def isPresent(cls, event: dict) -> bool:
+        return cls.KEY in event and event["action"] == "transferred"
+
+    def parse(self) -> str:
+        return self.header("issue transferred") + self.assignee_str.format(
+                url=self.event["issue"]["html_url"],
+                number=self.event["issue"]["number"],
+                target_link=self.event["changes"]["new_repository"]["html_url"],
+                target=self.event["changes"]["new_repository"]["full_name"]
+            )
