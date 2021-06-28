b3dcdff824a557ad40768e96c4fe113dbd4441bd
SurajMeena <surajmeena505@gmail.com>
2021-06-28 16:16:03 +0000

    added parsing of collaborator invite acceptation and removal of collaborator.....permission changes for collaborator not done due to lack of an organization account for testing

--- telegithook/events/__init__.py
+++ telegithook/events/__init__.py
@@ -1,8 +1,9 @@
 from .code import Commit, Fork
 from .issues import IssueOpened, IssueComment, IssueLabeled, IssueAssigned, IssueEditedTitle, IssueEditedBody, IssueDeleted, IssuePinned, IssueTransferred, IssueUnpinned, IssueClosed, IssueReopened, IssueUnassigned, IssueLocked, IssueUnlocked, IssueTransferred, IssueMilestoned, IssueDemilestoned
+from .collaborator import colabAdded, colabRemoved
 from ..env import EXCLUDE_EVENTS
 
-_events = [ Commit, Fork, IssueOpened, IssueComment, IssueLabeled, IssueAssigned, IssueEditedTitle, IssueEditedBody, IssueDeleted, IssuePinned, IssueUnpinned, IssueUnpinned, IssueClosed, IssueReopened, IssueUnassigned, IssueLocked, IssueUnlocked, IssueTransferred, IssueMilestoned, IssueDemilestoned ]
+_events = [ Commit, Fork, IssueOpened, IssueComment, IssueLabeled, IssueAssigned, IssueEditedTitle, IssueEditedBody, IssueDeleted, IssuePinned, IssueUnpinned, IssueUnpinned, IssueClosed, IssueReopened, IssueUnassigned, IssueLocked, IssueUnlocked, IssueTransferred, IssueMilestoned, IssueDemilestoned, colabAdded, colabRemoved ]
 events = []
 
 for event in _events:
