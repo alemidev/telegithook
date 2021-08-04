9e6df9a976ada51889b6c6cd0055e62059386d99
alemi <me@alemi.dev>
2021-08-05 01:37:18 +0200

    leftovers

--- telegithook/events/base.py
+++ telegithook/events/base.py
@@ -7,10 +7,6 @@ class EventBase(object):
     def __init__(self, event: dict):
         self.event = event
 
-    @classmethod
-    def isPresent(cls, event: dict) -> bool:
-        return cls.KEY in event
-
     def header(self, title : str = "") -> str:
         return STR["header"].format(
             repo=self.event['repository']['full_name'], # TODO repository is not guaranteed! maybe organization
@@ -32,52 +28,50 @@ class EventBase(object):
     def parse(self) -> str:
         return self.header() + self.body()
 
-class CheckRun(EventBase):
-    KEY = "check_run"
-
-class CheckSuite(EventBase):
-    KEY = "check_suite"
-
-class CodeScanningAlert(EventBase):
-    KEY = "alert"
+# class CheckRun(EventBase):
+#     KEY = "check_run"
+ 
+# class CheckSuite(EventBase):
+#     KEY = "check_suite"
+ 
+# class CodeScanningAlert(EventBase):
+#     KEY = "alert"
 
 # class CommitComment(EventBase):
 #     KEY = "comment"
 
-class ContentReference(EventBase):
-    KEY = "content_reference"
+# class ContentReference(EventBase):
+#     KEY = "content_reference"
 
 # class Create(EventBase):
 #     KEY = "ref"
-# 
+
 # class Delete(EventBase):
 #     KEY = "ref"
 
-class DeployKey(EventBase):
-    KEY = "key"
+# class DeployKey(EventBase):
+#     KEY = "key"
 
-class Deployment(EventBase):
-    KEY = "deployment"
+# class Deployment(EventBase):
+#     KEY = "deployment"
 
-class DeploymentStatus(EventBase):
-    KEY = "deployment_status"
+# class DeploymentStatus(EventBase):
+#     KEY = "deployment_status"
 
-class Discussion(EventBase):
-    KEY = "discussion"
+# class Discussion(EventBase):
+#     KEY = "discussion"
 
 # class DiscussionComment(EventBase):
 #     KEY = "comment"
 
-class Fork(EventBase):
-    KEY = "forkee"
+# class Fork(EventBase):
+#     KEY = "forkee"
 
 # class GithubAppAuthorization(EventBase):
 #     KEY = 
 
-class WikiPages(EventBase): # This is called "gollum" for some reason?
-    KEY = "pages"
+# class WikiPages(EventBase): # This is called "gollum" for some reason?
+#     KEY = "pages"
 
-class Installation(EventBase):
-    KEY = "installation"
-
-class 
+# class Installation(EventBase):
+#     KEY = "installation"
