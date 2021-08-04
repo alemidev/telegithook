f292516bf1d961343056fc9abe20457e90fe3817
alemi <me@alemi.dev>
2021-08-05 01:42:28 +0200

    fixed some keys

--- telegithook/events/__init__.py
+++ telegithook/events/__init__.py
@@ -3,7 +3,7 @@ from .issues import IssueOpened, IssueComment, IssueLabeled, IssueAssigned, Issu
 from .collaborator import colabAdded, colabRemoved
 from .milestone import milestoneCreated, milestoneDeleted, milestoneOpened, milestoneEdited, milestoneClosed
 from .branch import branchCreated, branchDeleted
-from base import EventBase
+from .base import EventBase
 from ..env import EXCLUDE_EVENTS
 
 _events = [branchCreated, branchDeleted, Commit, Fork, IssueOpened, IssueComment, IssueLabeled, IssueAssigned, IssueEditedTitle, IssueEditedBody, IssueDeleted, IssuePinned, IssueUnpinned, IssueUnpinned, IssueClosed, IssueReopened, IssueUnassigned, IssueLocked, IssueUnlocked, IssueTransferred, IssueMilestoned, IssueDemilestoned, colabAdded, colabRemoved, Starred, Unstarred, milestoneCreated, milestoneDeleted, milestoneOpened, milestoneEdited, milestoneClosed]
